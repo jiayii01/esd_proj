93b65243ccebb25eeea06af5caf2e3b69699e054
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2022-03-21 13:07:35 +0800

    Add files via upload

---
+++ job.py
@@ -0,0 +1,161 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import environ
+# from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/jobs'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# CORS(app)
+
+class Job(db.Model):
+    __tablename__ = 'jobs'
+
+    jobID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(10))
+    # dateTime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    deadline = db.Column(db.Date, nullable=False)
+    pickUpLocation = db.Column(db.String(1024), nullable=False)
+    destination = db.Column(db.String(1024), nullable=False)
+    freelancerID = db.Column(db.String(6))
+
+    def __init__(self, jobID, name, price, description, status, deadline, pickUpLocation, destination, freelancerID):
+        self.jobID = jobID
+        self.name = name
+        self.price = price
+        self.description = description
+        self.status = status
+        # self.dateTime = dateTime
+        self.deadline = deadline
+        self.pickUpLocation = pickUpLocation
+        self.destination = destination
+        self.freelancerID = freelancerID
+
+    def json(self):
+        return {"jobID": self.jobID, "name": self.name, "price": self.price, "description": self.description, "status": self.status, "deadline": self.deadline, "pickUpLocation": self.pickUpLocation, "destination": self.destination, "freelancerID": self.freelancerID}
+        # return {"jobID": self.jobID, "name": self.name, "price": self.price, "description": self.description, "status": self.status}
+
+@app.route("/job")
+def get_all():
+    joblist = Job.query.all()
+    if len(joblist):
+        return jsonify(
+            {
+              "code": 200,
+              "data": {
+                  "jobs": [job.json() for job in joblist]
+              }
+            }
+        )
+    return jsonify(
+        {
+          "code": 404,
+          "message": "There are no jobs."
+        }
+    ), 404
+
+@app.route("/job/<int:jobID>")
+def find_by_jobID(jobID):
+    job = Job.query.filter_by(jobID=jobID).first()
+    if job:
+        return jsonify(
+            {
+              "code": 200,
+              "data": job.json()
+            }
+        )
+    return jsonify(
+        {
+          "code": 404,
+          "message": "Job doesn't exist."
+        }
+    ), 404
+
+
+@app.route("/job/<int:jobID>", methods=['POST'])
+def create_job(jobID):
+    if (Job.query.filter_by(jobID=jobID).first()):
+        return jsonify(
+            {
+              "code": 400,
+              "data": {
+                  "jobID": jobID
+              },
+              "message": "Job already exists."
+            }
+        ), 400
+
+    data = request.get_json()
+    job = Job(jobID, **data)
+
+    try:
+        db.session.add(job)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+              "code": 500,
+              "data": {
+                  "jobID": jobID
+              },
+              "message": "An error occurred creating the job."
+            }
+        ), 500
+    # db.session.add(job)
+    # db.session.commit()
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": job.json()
+        }
+    ), 201
+
+@app.route("/job/<int:jobID>", methods=['PUT'])
+def update_job(jobID):
+    job = Job.query.filter_by(jobID=jobID).first()
+    if job:
+        data = request.get_json()
+        for col in data:
+          if col == "name":
+            job.name = data['name']
+          if col == "price":
+            job.price = data['price']
+          if col == "description":
+            job.description = data['description']
+          if col == "status":
+            job.status = data['status']
+          if col == "deadline":
+            job.deadline = data['deadline']
+          if col == "destination":
+            job.destination = data['destination']
+          if col == "freelancerID":
+            job.freelancerID = data['freelancerID']
+
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": job.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "jobID": jobID
+            },
+            "message": "Job not found."
+        }
+    ), 404
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ jobs.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 18 Mar, 2022
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `order`
+--
+CREATE DATABASE IF NOT EXISTS `jobs` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `jobs`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `jobID` int(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `price` float(4,2) NOT NULL DEFAULT 0,
+  `description` varchar(200) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'NEW',
+  `dateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deadline` date NOT NULL,
+  `pickUpLocation` varchar(1024) NOT NULL,
+  `destination` varchar(1024) NOT NULL,
+  `freelancerID` varchar(6),
+  PRIMARY KEY (`jobID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jobs`
+--
+INSERT INTO `jobs` (`jobID`, `name`, `price`, `description`, `status`,`dateTime`, `deadline`, `pickUpLocation`,`destination`,`freelancerID`) VALUES
+(1, 'Baked Goods Delivery', '12.00', 'I need you to deliver 2 orders of cupcakes to the destination.', 'NEW', '2022-03-18 0:14:55', '2022-04-01', '520123 Simei Street 1 Blk 123 #02-07','520123 Pasir Ris Street 7 Blk 321 #07-02','F1');
+
+INSERT INTO `jobs` (`jobID`, `name`, `price`, `description`, `status`,`dateTime`, `deadline`,`pickUpLocation`, `destination`, `freelancerID`) VALUES (2, 'Brownies Delivery', '25.00', 'I need you to delivery 1 box of lemon pie to the destination, thanks!', 'NEW', '2022-03-19 19:38:11', '2022-04-01','520345 Bedok Street 2 Blk 345 #04-08', '512567 Jurong Street 10 Blk 567 #11-32', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
