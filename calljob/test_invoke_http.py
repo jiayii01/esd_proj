ceffd7baa540b61afa31ea107036eb246f1776fe
jiayii01 <79521323+jiayii01@users.noreply.github.com>
2022-04-02 23:34:44 +0800

    calljob

---
+++ calljob/test_invoke_http.py
@@ -0,0 +1,31 @@
+from invokes import invoke_http
+from os import environ
+
+job_URL = environ.get('jobURL') or input("Enter Job service URL: ")  
+
+# invoke book microservice to get all books
+results = invoke_http(job_URL, method='GET')
+
+print( type(results) )
+print()
+print( results )
+
+# invoke book microservice to create a book
+jobID = 5
+job_details = {
+    "deadline": "2022-04-13",
+    "deliveryDate": "2022-04-15",
+    "description": "Please deliver these cakes!",
+    "destination": "765621 Marina Bay Sands 4",
+    "name": "Cake Delivery",
+    "pickUpLocation": "512456 Ubi Street 12 Blk 456 #11-28",
+    "price": 21.0,
+    "status": "NEW"
+}
+create_results = invoke_http(
+        job_URL + "/" + str(jobID), method='POST', 
+        json=job_details
+    )
+
+print()
+print( create_results )
