91ac184460985fb1b7869e35bbd622042f0c0468
xsquaree <35002684+xsquaree@users.noreply.github.com>
2022-04-03 02:39:51 +0800

    Add files via upload

--- accept_freelancer/accept_freelancer.py
+++ accept_freelancer/accept_freelancer.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 import os, sys
-
-import requests
 from invokes import invoke_http
-
-#import amqp_setup
+from invokesNotification import MyRequestClass
+import amqp_setup
 import pika
 import json
 
@@ -15,6 +12,7 @@ CORS(app)
 
 job_URL = "http://localhost:5001/jobs"
 bidding_URL = "http://localhost:5002/bidding"
+notification_URL = "http://localhost:5003/notification"
 
 
 @app.route("/accept_freelancer", methods=['POST'])   
@@ -37,6 +35,10 @@ def accept_freelancer():
             result =  updateJobStatus(jobChosen,freelancerChosen)
             print('\n------------------------')
             print('\nresult: ', result)
+
+            #4. Send notifications to the relevant stakeholder
+            send_notification(newStatus['data']["jobID"])
+
 
             return jsonify(result), result["code"]
 
@@ -75,9 +77,43 @@ def updateJobStatus(jobId,freelancerId):
     code = job_result["code"]
     message = json.dumps(job_result)
 
+    #amqp handling
     if code not in range(200, 300):
-        print("error")
+        # Inform the error microservice
+        #print('\n\n-----Invoking error microservice as update job status fails-----')
+        print('\n\n-----Publishing the (job error) message with routing_key=job.error-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="job.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        # make message persistent within the matching queues until it is received by some receiver 
+        # (the matching queues have to exist and be durable and bound to the exchange)
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails        
+        print("\njob status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), job_result)
+
+        # 7. Return error
+        return {
+            "code": 400,
+            "data": {
+                "order_result": job_result,
+            },
+            "message": "job error sent for error handling."
+        } 
+
     else: 
+        # success job update 
+        # record the activity log anyway
+        #print('\n\n-----Invoking activity_log microservice-----')
+        print('\n\n-----Publishing the (job info) message with routing_key=job.info-----')        
+
+        # invoke_http(activity_log_URL, method="POST", json=order_result)            
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="job.info", 
+            body=message)
+    
+        print("\njob published to RabbitMQ Exchange.\n")
+
         print("successful")
         return job_result
 
@@ -98,8 +134,40 @@ def updateBiddingStatus(bidding):
     message = json.dumps(bidding_result)
 
     if code not in range(200, 300):
-        print("error")
+        # Inform the error microservice
+        #print('\n\n-----Invoking error microservice as update bidding status fails-----')
+        print('\n\n-----Publishing the (bidding error) message with routing_key=bidding.error-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="bidding.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        # make message persistent within the matching queues until it is received by some receiver 
+        # (the matching queues have to exist and be durable and bound to the exchange)
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails        
+        print("\nbidding status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), bidding_result)
+
+        # 7. Return error
+        return {
+            "code": 400,
+            "data": {
+                "order_result": bidding_result,
+            },
+            "message": "job error sent for error handling."
+        } 
     else: 
+        # success job update 
+        # record the activity log anyway
+        #print('\n\n-----Invoking activity_log microservice-----')
+        print('\n\n-----Publishing the (bidding info) message with routing_key=bidding.info-----')        
+
+        # invoke_http(activity_log_URL, method="POST", json=order_result)            
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="bidding.info", 
+            body=message)
+    
+        print("\nbidding published to RabbitMQ Exchange.\n")
+
         print("successful")
         return bidding_result
 
@@ -121,56 +189,10 @@ def chosenBidding(id):   #retrieve bidding info that we chose by bidding ID
         return bidding_result
     
 
-"""""
-
-for amqp
-    if code not in range(200, 300):
-        # Inform the error microservice
-        #print('\n\n-----Invoking error microservice as order fails-----')
-        print('\n\n-----Publishing the (job error) message with routing_key=job.error-----')
-
-        # invoke_http(error_URL, method="POST", json=order_result)
-        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.error", 
-            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
-        # make message persistent within the matching queues until it is received by some receiver 
-        # (the matching queues have to exist and be durable and bound to the exchange)
-
-        # - reply from the invocation is not used;
-        # continue even if this invocation fails        
-        print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(
-            code), job_result)
-
-        # 7. Return error
-        return {
-            "code": 500,
-            "data": {"order_result": job_result},
-            "message": "Order creation failure sent for error handling."
-        }
-
-    # Notice that we are publishing to "Activity Log" only when there is no error in order creation.
-    # In http version, we first invoked "Activity Log" and then checked for error.
-    # Since the "Activity Log" binds to the queue using '#' => any routing_key would be matched 
-    # and a message sent to “Error” queue can be received by “Activity Log” too.
-
-    else:
-        # 4. Record new order
-        # record the activity log anyway
-        #print('\n\n-----Invoking activity_log microservice-----')
-        print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
-
-        # invoke_http(activity_log_URL, method="POST", json=order_result)            
-        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.info", 
-            body=message)
-  
-    print("\nOrder published to RabbitMQ Exchange.\n")
-    # - reply from the invocation is not used;
-    # continue even if this invocation fails
-    
-    # 5. Send new order to shipping
-    # Invoke the shipping record microservice
-    print('\n\n-----Invoking shipping_record microservice-----')    
-"""""  
-    
+def send_notification(jobID):
+    print('\n-----Invoking notification microservice-----')
+    invoke_http(notification_URL+"/"+str(jobID), method='GET')
+    print("doneeee")
 
 
 # Execute this program if it is run as a main script (not by 'import')
